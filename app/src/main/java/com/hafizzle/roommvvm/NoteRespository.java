07a7deaf9af21119f39593c36e42c293083db670
Hafiz Hamidi <Hafizbhamidi@gmail.com>
2020-03-22 22:31:32 -0500

    Add clean API note repository
    Add clean API for view model to make calls to database.

--- app/src/main/java/com/hafizzle/roommvvm/NoteRespository.java
+++ app/src/main/java/com/hafizzle/roommvvm/NoteRespository.java
@@ -1,9 +1,103 @@
 package com.hafizzle.roommvvm;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class NoteRespository {
     /*
     Provides clean API to allow the rest of the View Model to access database.
-     */
 
-    public NoteRespository
+    Room does not allow database operations on the main thread.
+     */
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    //Gets instance of database
+    public NoteRespository(Application application){
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao();
+        allNotes = noteDao.getAllNotes();
+    }
+
+    public void insert(Note note){
+        new InsertNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void update(Note note){
+        new UpdateNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void delete(Note note){
+        new DeleteNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void deleteAllNotes(){
+        new DeleteAllNotesAsyncTask(noteDao).execute();
+    }
+
+    //Room handles background operations for database on LiveData background thread.
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private InsertNoteAsyncTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private UpdateNoteAsyncTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private DeleteNoteAsyncTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private DeleteAllNotesAsyncTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+
 }
